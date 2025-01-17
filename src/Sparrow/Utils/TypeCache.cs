9e793909cfaed81dc67f6a9b20d6039da558a6c2
Jon2G <jonyjouna@gmail.com>
2025-01-16 20:01:09 -0600

    Use absolute value of types hashcode on TypeCache

--- src/Sparrow/Utils/TypeCache.cs
+++ src/Sparrow/Utils/TypeCache.cs
@@ -18,7 +18,7 @@ namespace Sparrow.Utils
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool TryGet(Type type, out T result)
         {
-            int typeHash = type.GetHashCode();
+            int typeHash = Math.Abs(type.GetHashCode());
 
             // We get the data and after that we always work from there to avoid
             // harmful race conditions.
@@ -62,7 +62,7 @@ namespace Sparrow.Utils
 
         public void Put(Type type, T value)
         {
-            int typeHash = type.GetHashCode();
+            int typeHash = Math.Abs(type.GetHashCode());
             int bucket = typeHash % _size;
 
             // The idea is that this TypeCache<T> is thread safe. It is better to lose some Put
